0bb7c96e446dc9a0d6523f60857ace09408f25f4
JUNGCHAEYOON <108243290+JUNGCHAEYOON@users.noreply.github.com>
2023-07-17 17:05:08 +0900

    0717 commit
    88 까지 완료

---
+++ 03_Android/android81_Localization/app/src/main/java/com/test/android81_localization/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.test.android81_localization
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.test.android81_localization.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var activityMainBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        activityMainBinding.run{
+            textView2.setText(R.string.str1)
+            imageView2.setImageResource(R.drawable.flag1)
+        }
+
+        setContentView(activityMainBinding.root)
+    }
+}
+
+
+
+
+
+
+
+
+
+
